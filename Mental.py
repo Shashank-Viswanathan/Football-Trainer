6b20d9d48f96bde64ee1ffc35946b90a62b9d64c
Shashank-Viswanathan <147416056+Shashank-Viswanathan@users.noreply.github.com>
2023-10-18 21:54:33 -0400

    Update Mental.py
    Nothing

--- Mental.py
+++ Mental.py
@@ -3,6 +3,7 @@ from colorama import Fore
 drills_m = dict()
 drills_m_func = list()
 # Difficulty; Materials; position; amount of people
+#start
 drills_m["1. Timed Wall Pass"] = "Easy; Ball, Wall(s), Timer; Midfield; 1 person minimum"
 def Timed_Wall_Pass():
   print(Fore.YELLOW+"\nIn this drill, you set a timer for 1 minute and pass the ball to a wall, let it bounce back and pass to a different part or a different wall. Try to quickly decide what you are passing to increase how many passes you can complete. Don't randomly pass!")
@@ -13,4 +14,4 @@ def Timed_Wall_Pass():
   score = input("How many passes did you complete? (Try to beat your score!)\t")
   print(Fore.GREEN+"\nYou completed " + score + " passes in 1 minute!")
 drills_m_func.append(Timed_Wall_Pass)
-#
+#end
