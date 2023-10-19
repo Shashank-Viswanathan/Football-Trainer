594c22132d3969cd991b67d771b35736692b8624
Shashank-Viswanathan <147416056+Shashank-Viswanathan@users.noreply.github.com>
2023-10-18 21:51:28 -0400

    Update and rename Mental.py to file2.py
    Template

---
+++ file2.py
@@ -0,0 +1,12 @@
+import time, random
+from colorama import Fore
+drills_f2 = dict()
+drills_f2_func = list()
+# Description template here
+# start
+drills_m["# and Drill name"] = "Description"
+def drill_name():
+    print("Content")
+drills_m_func.append(drill_name)
+# end
+# Add more dills
