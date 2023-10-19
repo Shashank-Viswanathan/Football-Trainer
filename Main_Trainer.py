e2db7e626a3a59230bb878798a5d68ce8f66a6a3
Shashank-Viswanathan <147416056+Shashank-Viswanathan@users.noreply.github.com>
2023-10-18 21:14:54 -0400

    Update Main_Trainer.py
    Template

--- Main_Trainer.py
+++ Main_Trainer.py
@@ -1,32 +1,33 @@
 import time, random
 from colorama import Fore
-import Mental as m
-import Physical as p
-import Technical as t
+import file1 as f1#edit
+import file2 as f2#edit
+import file3 as f3#edit
+#You can add more files
 while True:
-    print(Fore.BLUE+"Football Trainer - OXH Studios")
-    sections = ["Physical", "Mental", "Technical"]
+    print(Fore.BLUE+"Sports Trainer - OXH Studios")#edit
+    sections = ["file1", "file2", "file3"]#edit
     print("\nWhich ability would you like to train? (Type number...)")
     print(sections)
     ability = int(input())
     try:
         print("\n" + str(sections[ability - 1]))
         print(Fore.YELLOW+"\nDrills:")
-        if ability == 1:
-            for drill in p.drills_p:
-                print("\t" + str(drill) + " : " + str(p.drills_p[drill]))
+        if ability == 1:#edit
+            for drill in f1.drills_f1:
+                print("\t" + str(drill) + " : " + str(f1.drills_f1[drill]))
                 drill_choice = int(input("\nWhich drill would you like to do (#)?\t"))
-                p.drills_p_func[drill_choice-1]()
-        elif ability == 2:
-            for drill in m.drills_m:
-                print("\t" + str(drill) + " : " + str(m.drills_m[drill]))
+                f1.drills_f1_func[drill_choice-1]()
+        elif f2 == 2:#edit
+            for drill in f2.drills_m:
+                print("\t" + str(drill) + " : " + str(f2.drills_f2[drill]))
                 drill_choice = int(input("\nWhich drill would you like to do (#)?\t"))
-                m.drills_m_func[drill_choice-1]()
-        elif ability == 3:
-            for drill in t.drills_t:
-                print("\t" + str(drill) + " : " + str(t.drills_t[drill]))
+                f2.drills_f2_func[drill_choice-1]()
+        elif ability == 3:#edit
+            for drill in f3.drills_f3:
+                print("\t" + str(drill) + " : " + str(f3.drills_f3[drill]))
                 drill_choice = int(input("\nWhich drill would you like to do (#)?\t"))
-                t.drills_t_func[drill_choice-1]()
+                f3.drills_f3_func[drill_choice-1]()
     except:
         print(Fore.RED+"\nInvalid Input\n")
         continue #restarts program
