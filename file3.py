38d3b26162ebb74a3adb8e781b88dc5754fe53f7
Shashank-Viswanathan <147416056+Shashank-Viswanathan@users.noreply.github.com>
2023-10-18 21:53:13 -0400

    Update and rename Technical.py to file3.py
    Template

---
+++ file3.py
@@ -0,0 +1,12 @@
+import time, random
+from colorama import Fore
+drills_f3 = dict()
+drills_f3_func = list()
+# Description template here
+# start
+drills_f3["# and Drill name"] = "Description"
+def drill_name():
+    print("Content")
+drills_f3_func.append(drill_name)
+# end
+# Add more dills
