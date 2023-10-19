d1f3f43c29747beb4bfe110dd94bfa7328225dc6
Shashank-Viswanathan <147416056+Shashank-Viswanathan@users.noreply.github.com>
2023-10-18 21:52:30 -0400

    Update and rename Physical.py to file1.py
    Template

---
+++ file1.py
@@ -0,0 +1,12 @@
+import time, random
+from colorama import Fore
+drills_f1 = dict()
+drills_f1_func = list()
+# Description template here
+# start
+drills_f1["# and Drill name"] = "Description"
+def drill_name():
+    print("Content")
+drills_f1_func.append(drill_name)
+# end
+# Add more dills
